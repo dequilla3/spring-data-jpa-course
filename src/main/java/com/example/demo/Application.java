0de2c51ab3cba5c8ad16f7fe8f0c22b801e17c1e
Kim Dequilla <67781364+dequilla3@users.noreply.github.com>
2022-01-08 20:10:36 +0800

    jpa-course

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
+  @Bean
+  CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+    return args -> {
+      Student student = new Student();
+      student.setFirstName("Kim Ariel");
+      student.setLastName("Dequilla");
+      student.setEmail("deqkim4@gmail.com");
+      student.setAge(23);
+      studentRepository.save(student);
+    };
+  }
 }
